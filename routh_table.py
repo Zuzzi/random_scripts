e1dd83c789df5b586d09b6c028939c5548cec320
Carlo Peroni <carloperoni.italy@gmail.com>
2016-10-20 17:17:38 +0200

    It generates up to 3 rows

--- routh_table.py
+++ routh_table.py
@@ -1,74 +1,66 @@
 # Routh stability criterion
 
+def build_routh_table(coefficients_list):
+	table = []
+	first_row = []
+	second_row = []
+	first_row.append(1)
+	for index, c in enumerate(coefficients_list):
+		if index % 2 == 0:
+			second_row.append(c)
+		else:
+			first_row.append(c)
+	print(first_row)
+	print(second_row)
+	table.append(first_row)
+	table.append(second_row)
+	dimension_new_row = len(second_row) - 1
+	third_row = []
+	for i in range(dimension_new_row):
+		matrix = build_matrix(table, i)
+		print(matrix)
+		print(det(matrix))
+		element_i = (-1/table[1][0]) * det(matrix)
+		third_row.append(element_i)
+	table.append(third_row)
+	return table
 
 
-def get_first_exponent(expression):
-	exponent = ""
-	found_exp = False
-	for char in expression:
-		if char.isdigit() or char == "-":
-			exponent += char
-			found_exp = True
-		elif found_exp:
-			break
-	return int(exponent)
+def build_matrix(table, index):
+	matrix = []
+	first_row = []
+	second_row = []
+	a0 = table[0][0]
+	a1 = table[0][index + 1]
+	a2 = table[1][0]
+	a3 = table[1][index + 1]
+	first_row.extend([a0, a1])
+	second_row.extend([a2, a3])
+	matrix.extend([first_row, second_row])
+	return matrix
 
 
-
-def get_coefficients (expression):
-	# find the first exponent (dimension of the polynomial)
-	dimension = get_first_exponent(expression)
-	print("Dimension = ", dimension)
-	# find the coefficients
-	for index, char in enumerate(expression):
-		if char == "+" or char == "-": # coefficient spot starts here!
-			exponent = get_first_exponent(expression[index + 1:])
-			print("Exponent = ", exponent)
-			
-			
-
-	
-	
-			
-			
-			
-	
-	
-
-
-
-
-
-
-
+def det(matrix):
+	det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+	return det
 
 
 def main():
-    print("Routh table generator")
-    print("Type in the polynomial P(y) = y^n + ay^(n-1) + by^(n-2) +....+ z")
-    expression = input()
-    #coefficients_list = get_coefficients(expression)
-    get_coefficients(expression)
-    
+	print("Routh table generator of P(y) = y^n + ay^(n-1) + by^(n-2) +....+ z")
+	print("Type in the coefficients of P(y), from z to a, separated by whitespace")
+	expression = input()
+	coefficients_list = expression.split()
+	coefficients_list = [int(i) for i in coefficients_list]
+	print(coefficients_list)
+	table = build_routh_table(coefficients_list)
+	print("The final table is:")
+	print(table)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			
 
 
 
 
 if __name__ == "__main__":
-    main()
+	main()
 
