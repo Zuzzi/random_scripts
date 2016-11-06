fff97119d598eb89e716c1476d4bfc0218992103
Carlo Peroni <carloperoni.italy@gmail.com>
2016-11-06 22:49:18 +0100

    first attempt of matplotlib feature

--- iLift.py
+++ iLift.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker
 import datetime
 from sqlalchemy_declarative import Workout, Base
 import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 
 def start_session():
@@ -32,6 +34,40 @@ def show_workouts():
 		print("id = " + str(workout.id) + ", type = " + workout.type + ", volume = " + str(workout.volume) +
 		      ", max_kg = " + str(workout.max_kg) + ", date = " + str(workout.date))
 	print("All workouts displayed!")
+	"""x1_list = []
+	x2_list = []
+	y1_list = []
+	y2_list = []"""
+	
+	
+	x1_list = [datetime.date(2016, 10, 30), datetime.date(2016, 10, 31), datetime.date(2016, 11, 2), datetime.date(2016, 11, 6), datetime.date(2016, 11, 10), datetime.date(2016, 11, 12)]
+	x2_list = [datetime.date(2016, 11, 1), datetime.date(2016, 11, 3), datetime.date(2016, 11, 4), datetime.date(2016, 11, 5), datetime.date(2016, 11, 8), datetime.date(2016, 11, 9)]
+	y1_list = [4000, 4230, 4300, 4050, 4203, 4030]
+	y2_list = [6020, 6340, 6200, 6400, 6500, 6040]
+	
+
+	
+
+	"""for workout in all_workouts:
+		if workout.type == "log_lift":
+			x1_list.append(workout.date)
+			y1_list.append(workout.volume)
+		elif workout.type == "bench_press":
+			x2_list.append(workout.date)
+			y2_list.append(workout.volume)
+	"""
+	
+	fig = plt.figure() 
+	fig.canvas.set_window_title('iLift') 
+	plt.plot(x1_list,y1_list, label = "log lift")
+	plt.plot(x2_list, y2_list, label = "bench press")
+	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
+	plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+	plt.title('Volume')
+	plt.legend()
+	plt.show()
+	plt.gcf().autofmt_xdate()
+	print("Chart displayed!")
 	
 
 def delete_workout():
